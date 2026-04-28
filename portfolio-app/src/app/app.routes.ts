fed649cbbc8e96890b51b1b5c8e2366aac36cb3e
Jettberg <jettmberg@gmail.com>
2026-04-28 10:40:03 -0500

    added progression component and updated routing

--- portfolio-app/src/app/app.routes.ts
+++ portfolio-app/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { HomePageComponent } from './pages/home/home-page.component';
 import { AboutPageComponent } from './pages/about/about-page.component';
 import { ProjectsPageComponent } from './pages/projects/projects-page.component';
 import { ContactPageComponent } from './pages/contact/contact-page.component';
+import { ProgressionPageComponent } from './pages/progression/progression-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'about', component: AboutPageComponent },
   { path: 'projects', component: ProjectsPageComponent },
+  { path: 'progression', component: ProgressionPageComponent },
   { path: 'contact', component: ContactPageComponent },
 ];
